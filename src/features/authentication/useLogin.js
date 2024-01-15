bb514f1cb2264cfcdf7cf8651278ea542c2300f6
djswalterix <walter45hoxha@gmail.com>
2024-01-15 21:09:52 +0100

    bug fix cache login

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -10,7 +10,7 @@ export function useLogin() {
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: (user) => {
       navigate("/dashboard", { replace: true });
-      queryClient.setQueriesData(["user"], user);
+      queryClient.setQueryData(["user"], user.user);
     },
     onError: (err) => {
       console.log("Error", err);
